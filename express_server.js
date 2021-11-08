db9e84cc18d7c0330a4b25125b6c1e5d9b6b13a2
BRB3D <samuraix221@hotmail.com>
2021-11-08 17:28:36 -0500

    route to new template where it displays the long url and the short url as a link. the new template was created in the views folder and there is no need to reference it in render as views/urls_show becasue it check there by deault.

--- express_server.js
+++ express_server.js
@@ -24,6 +24,13 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hellow <b>World</b></body></html>\n');
 });
 
+//new route to render urls_show,ejs template.
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[`${req.params.shortURL}`]};
+  res.render("urls_show", templateVars);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
