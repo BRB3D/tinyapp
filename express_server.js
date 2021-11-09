254825fb186552f5cf02a31ed3899248122e43d2
BRB3D <samuraix221@hotmail.com>
2021-11-08 20:44:39 -0500

    added a new get before /urls/:shortURL that will deal with the creation of new timy URLs. The order is important and an explanation has been commented in the express_server.js file too.

--- express_server.js
+++ express_server.js
@@ -24,11 +24,17 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hellow <b>World</b></body></html>\n');
 });
 
+//new route to render a form for new urls, this must be declared before /urls/:shortURL or the calls to /urls/new will be handled by /urls/:shortURL. 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new')
+})
+
 //new route to render urls_show,ejs template.
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[`${req.params.shortURL}`]};
   res.render("urls_show", templateVars);
 });
+
 
 
 app.listen(PORT, () => {
