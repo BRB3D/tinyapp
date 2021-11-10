dde85df365eb7b2335233d1e68141df19677bbe0
BRB3D <samuraix221@hotmail.com>
2021-11-10 14:12:12 -0500

    template for registering created as urls-registration.ejs in views. added a GET to display this form in express_server.js also organized express server to be more readable;

--- express_server.js
+++ express_server.js
@@ -28,13 +28,14 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hellow <b>World</b></body></html>\n');
 });
 
+//*RENDER index page
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase, username: req.cookies['username']};
   res.render('urls_index', templateVars);
 });
 
 
-//new route to render a form for new urls, this must be declared before /urls/:shortURL or the calls to /urls/new will be handled by /urls/:shortURL.
+//*RENDER New URLS this must be declared before /urls/:shortURL or the calls to /urls/new will be handled by /urls/:shortURL.
 app.get('/urls/new', (req, res) => {
   const templateVars = {username: req.cookies['username']};
   res.render('urls_new', templateVars);
@@ -42,14 +43,19 @@ app.get('/urls/new', (req, res) => {
 
 //--------------------------------------------------------------------------------------------//
 
-//new route to render urls_show,ejs template.
+//*RENDER urls_registration.
+app.get("/register", (req, res) => {
+
+  res.render("urls_registration");
+});
+
+//*RENDER urls_show,ejs 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[`${req.params.shortURL}`], username: req.cookies['username']};
   res.render("urls_show", templateVars);
 });
 
-
-//post request from urls_new.ejs. this handles with the response from that post form.
+//POST from urls_new.ejs creates random string and rediredts to /urls/shortURL ***************
 app.post("/urls", (req, res) => {
   const templateVars = urlDatabase;
   let short = generateRandomString();
@@ -68,7 +74,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${short}`);
 });
 
-//redirects to the long URL
+//RDIRECTS to Actual web page if it extists. 
 app.get("/u/:shortURL", (req, res) => {
   let long;
   const templateVars = urlDatabase;
@@ -79,20 +85,20 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 
-//Deals with username handling.
+//POST from login ****************
 app.post('/login', (req, res) => {
   const userName = req.body.username;
   res.cookie('username', userName,);
   res.redirect('/urls');
 });
 
-//Deals with username handling.
+//POST from logout *****************
 app.post('/logout', (req, res) => {
   res.clearCookie('username');
   res.redirect('/urls');
 });
 
-//deletes the value from the Database
+//POST deletes the value from the Database ********************
 app.post("/urls/:shortURL/delete", (req, res) => {
   const templateVars = urlDatabase;
   if (templateVars.hasOwnProperty(req.params.shortURL)) {
@@ -101,7 +107,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   }
 });
 
-//recieves data from url_show.jes and deals with edditing the long URL
+//POST from url_show.jes and deals with edditing the long URL
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.updatedURL;
   res.redirect("/urls");
